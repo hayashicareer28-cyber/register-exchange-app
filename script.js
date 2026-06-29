c72f4570e1f4cb8a223250664d6e449eec313cca
hayashicareer28-cyber <hayashi.career28@gmail.com>
2026-06-29 16:58:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -380,9 +380,9 @@ function createExternalExchangeSummary(adjustedCounts) {
   };
 
   const moneyRules = [
-    { moneyType: 5000, lowerLimit: 9 },
-    { moneyType: 1000, lowerLimit: 34 },
-    { moneyType: 500, lowerLimit: 9 }
+    { moneyType: 5000, lowerLimit: 10 },
+    { moneyType: 1000, lowerLimit: 38 },
+    { moneyType: 500, lowerLimit: 10 }
   ];
 
   for (const registerNumber of registers) {
